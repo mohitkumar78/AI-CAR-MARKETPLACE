00bc9351773519dc15fb85a758cddd1369c58132
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-03-22 21:25:24 +0530

    rebuilt the project

--- my-app/app/(auth)/layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function layout({ children }) {
-  return (
-    <div className="flex items-center justify-center h-screen">{children}</div>
-  );
-}
-
-export default layout;
